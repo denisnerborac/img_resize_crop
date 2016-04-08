dba0c0446af41d55339552cd6f8728b4a8d3acb9
Webforce3 <denis.nerborac@wf3.fr>
2016-04-08 14:46:39 +0200

    Update watermark - Add alpha support for border color

--- watermark.php
+++ watermark.php
@@ -3,9 +3,9 @@
 header('Content-type: image/png');
 $png_image = imagecreate(150, 150);
 
-imagecolorallocate($png_image, 15, 142, 210);
+imagecolorallocate($png_image, 0,0,0);
 imagesetthickness($png_image, 5);
-$black = imagecolorallocate($png_image, 0, 0, 0);
+$black = imagecolorallocatealpha($png_image, 255, 255, 255, 1);
 
 $x = 0;
 $y = 0;
@@ -19,7 +19,10 @@ imageline($png_image, $x+$w, $y, $x+$w, $y+$z, $black);
 imageline($png_image, $x, $y+$z, $x+$w ,$y+$z, $black);
 
 //diagonal
-imagedashedline($png_image, 150, 0, 0, 150, $black);
+imageline($png_image, $x1 = 150, $y1 = 0, $x2 = 0, $y2 = 150, $black);
+imageline($png_image, $x1 = 0, $y1 = 0, $x2 = 150, $y2 = 150, $black);
+imageline($png_image, $x1 = 0, $y1 = 75, $x2 = 150, $y2 = 75, $black);
+imageline($png_image, $x1 = 75, $y1 = 0, $x2 = 75, $y2 = 150, $black);
 
 imagepng($png_image);
 imagedestroy($png_image);
