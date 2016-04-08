0d8f6f69c4acc67383b7525eae123904edc0a31a
Webforce3 <denis.nerborac@wf3.fr>
2016-04-08 16:04:32 +0200

    Update watermark - Add function + optional source/dest image

--- watermark.php
+++ watermark.php
@@ -1,28 +1,56 @@
 <?php
 
-header('Content-type: image/png');
-$png_image = imagecreate(150, 150);
+function watermark($source_path = '', $width = 0, $height = 0, $line_width = 1, $bg_color_rgb = array(0,0,0), $line_color_rgb = array(255,255,255), $dest_path = '', $border = true) {
 
-imagecolorallocate($png_image, 0,0,0);
-imagesetthickness($png_image, 5);
-$black = imagecolorallocatealpha($png_image, 255, 255, 255, 1);
+	list($bg_color_red, $bg_color_green, $bg_color_blue) = $bg_color_rgb;
+	list($line_color_red, $line_color_green, $line_color_blue) = $line_color_rgb;
 
-$x = 0;
-$y = 0;
-$w = imagesx($png_image) - 1;
-$z = imagesy($png_image) - 1;
+	if (!empty($source_path) && file_exists($source_path)) {
 
-//borders
-imageline($png_image, $x, $y, $x, $y+$z, $black);
-imageline($png_image, $x, $y, $x+$w, $y, $black);
-imageline($png_image, $x+$w, $y, $x+$w, $y+$z, $black);
-imageline($png_image, $x, $y+$z, $x+$w ,$y+$z, $black);
+		list($width, $height, $type) = getimagesize($source_path);
 
-//diagonal
-imageline($png_image, $x1 = 150, $y1 = 0, $x2 = 0, $y2 = 150, $black);
-imageline($png_image, $x1 = 0, $y1 = 0, $x2 = 150, $y2 = 150, $black);
-imageline($png_image, $x1 = 0, $y1 = 75, $x2 = 150, $y2 = 75, $black);
-imageline($png_image, $x1 = 75, $y1 = 0, $x2 = 75, $y2 = 150, $black);
+		switch ($type) {
+			case IMAGETYPE_JPEG:
+				$image = imagecreatefromjpeg($source_path);
+			break;
+			case IMAGETYPE_PNG:
+				$image = imagecreatefrompng($source_path);
+			break;
+		}
+	} else {
+		$image = imagecreate($width, $height);
+	}
 
-imagepng($png_image);
-imagedestroy($png_image);
+	imagecolorallocate($image, $bg_color_red,$bg_color_green,$bg_color_blue);
+	imagesetthickness($image, $line_width);
+	$black = imagecolorallocatealpha($image, $line_color_red, $line_color_green, $line_color_blue, 1);
+
+	$x = 0;
+	$y = 0;
+	$w = $width - 1; // width
+	$h = $height - 1; // height
+
+	//borders
+	if ($border) {
+		imageline($image, $x, $y, $x, $y+$h, $black);
+		imageline($image, $x, $y, $x+$w, $y, $black);
+		imageline($image, $x+$w, $y, $x+$w, $y+$h, $black);
+		imageline($image, $x, $y+$h, $x+$w ,$y+$h, $black);
+	}
+
+	//imageline($image, $x1, $y1, $x2, $y2, $color)
+	imageline($image, $width, 0, 0, $height, $black);
+	imageline($image, 0, 0, $width, $height, $black);
+	imageline($image, 0, $height / 2, $width, $height / 2, $black);
+	imageline($image, $width / 2, 0, $width / 2, $height, $black);
+
+	if (empty($dest_path)) {
+		header('Content-type: image/png');
+		imagepng($image);
+	} else {
+		imagepng($image, $dest_path);
+	}
+	imagedestroy($image);
+}
+
+watermark('img/image.jpeg', 500, 250, 1, array(0,0,0), array(255,255,255), '', false);
