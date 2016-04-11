4a17376aea76dfbe9db06c531135fc4465810531
Denis Nerborac <denis.nerborac@gmail.com>
2016-04-11 02:06:57 +0200

    Move execution in resize.php to avoid unwanted resize

---
+++ resize.php
@@ -0,0 +1,13 @@
+<?php
+require 'Image.class.php';
+
+$resized_image = Image::resize($img_source = 'img/image.jpeg', $img_dest = 'img/image-thumb', $width = 200, $height = 200, $percent = 0, $crop = true, $format = IMAGETYPE_PNG);
+
+?>
+<h1>Resize and Crop image</h1>
+
+<h2>Before</h2>
+<img src="img/image.jpeg" />
+<hr>
+<h2>After</h2>
+<img src="<?= $resized_image ?>" />
