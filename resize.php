662bbdb308e656c740a22d3f242014396a8bd960
Denis Nerborac <denis.nerborac@gmail.com>
2016-04-11 02:11:37 +0200

    Update resize demo picture with

--- resize.php
+++ resize.php
@@ -7,7 +7,7 @@ $resized_image = Image::resize($img_source = 'img/image.jpeg', $img_dest = 'img/
 <h1>Resize and Crop image</h1>
 
 <h2>Before</h2>
-<img src="img/image.jpeg" />
+<img src="<?= $img_source ?>" />
 <hr>
 <h2>After</h2>
 <img src="<?= $resized_image ?>" />
